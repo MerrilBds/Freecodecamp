13a8fa0172f397f874801d850835ee5d8e297605
MerrilBds <Bdsmerril@gmail.com>
2023-07-04 23:02:47 +0200

    leçon 7 fin

---
+++ #7.variables(undefined)/declare.js
@@ -0,0 +1,13 @@
+/*
+Initialize the three variables a, b, a
+nd c with 5, 10, and "I am a" 
+respectively so that they will not be undefined.
+*/
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+// Only change code above this line
+a = a + 1;
+b = b + 5;
+c = c + " String!";
