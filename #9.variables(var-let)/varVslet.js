c884377f3421083f829fa476a3ba3bfbcce10ed9
MerrilBds <Bdsmerril@gmail.com>
2023-07-04 23:14:17 +0200

    leçon 9 fin

---
+++ #9.variables(var-let)/varVslet.js
@@ -0,0 +1,13 @@
+/*
+unlike var, 
+when you use let, 
+a variable with the same name can only be declared once.
+
+let camper = "James";
+let camper = "David";
+
+The error can be seen in your browser console.
+*/
+//Update the code so it only uses the let keyword.
+let catName = "Oliver";
+let catSound = "Meow!";
