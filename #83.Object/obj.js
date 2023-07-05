2de0d7301f45f8b50c597cfddf85f293494dd8fc
MerrilBds <Bdsmerril@gmail.com>
2023-07-06 00:27:49 +0200

    leçon 83 fin

---
+++ #83.Object/obj.js
@@ -0,0 +1,15 @@
+/*
+Objects are similar to arrays, 
+except that instead of using indexes to access and modify their data, 
+you access the data in objects through what are called properties.
+
+Objects are useful for storing data in a structured way, and can represent real world objects, like a cat.
+*/
+const myDog = {
+
+    name: "Wally",
+    legs: 4,
+    tails: 1,
+    friends: [2],
+
+};
