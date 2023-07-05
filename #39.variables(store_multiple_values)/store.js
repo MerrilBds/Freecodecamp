7c4faa771d4e781f271a6add08515c233cba6c04
MerrilBds <Bdsmerril@gmail.com>
2023-07-05 15:44:29 +0200

    leçon 39 fin

---
+++ #39.variables(store_multiple_values)/store.js
@@ -0,0 +1,10 @@
+/*
+
+With JavaScript array variables, we can store several pieces of data in one place.
+
+You start an array declaration with an opening square bracket, end it with a closing square bracket, and put a comma between each entry, like this:
+
+const sandwich = ["peanut butter", "jelly", "bread"];
+
+*/
+const myArray = ["myAge", 23];
