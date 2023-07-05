cc86094ee1218055e64bc38fcbda491d3fff18ed
MerrilBds <Bdsmerril@gmail.com>
2023-07-05 18:10:59 +0200

    leçon 54 fin

---
+++ #54.function-local_scopeVsglobal_scope/scope.js
@@ -0,0 +1,14 @@
+/*
+It is possible to have both local and global variables with the same name.
+ When you do this, the local variable takes precedence over the global variable.
+*/
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+  
+    const outerWear = "sweater";
+    return outerWear;
+   
+}
+
+myOutfit();
