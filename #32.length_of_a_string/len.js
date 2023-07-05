69e9472667ae2366e6053a5c75430efbe62e4bb7
MerrilBds <Bdsmerril@gmail.com>
2023-07-05 14:54:38 +0200

    leçon 32 fin

---
+++ #32.length_of_a_string/len.js
@@ -0,0 +1,7 @@
+// You can find the length of a String value by writing .length after the string variable or string literal.
+
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+lastNameLength = lastName.length;
+console.log(lastNameLength);
