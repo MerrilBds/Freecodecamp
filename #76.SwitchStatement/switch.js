7414af8278df6399f551a790f89a13821d7d9371
MerrilBds <Bdsmerril@gmail.com>
2023-07-05 21:51:43 +0200

    leçon 76 fin

---
+++ #76.SwitchStatement/switch.js
@@ -0,0 +1,25 @@
+/*
+1 - alpha
+2 - beta
+3 - gamma
+4 - delta
+*/
+function caseInSwitch(val) {
+    let answer = "";
+    switch (val) {
+        case 1:
+            answer += "alpha"
+            break;
+        case 2:
+            answer += "beta"
+            break;
+        case 3:
+            answer += "gamma";
+            break;
+        case 4:
+            answer += "delta";
+            break;
+    }
+    return answer;
+}
+caseInSwitch(1);
