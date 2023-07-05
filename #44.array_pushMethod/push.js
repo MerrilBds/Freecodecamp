95ccea73328c0e04825f0e0e82676d0b7f71de77
MerrilBds <Bdsmerril@gmail.com>
2023-07-05 16:31:55 +0200

    leçon 44 fin

---
+++ #44.array_pushMethod/push.js
@@ -0,0 +1,9 @@
+/*
+An easy way to append data to the end of an array is via the push() function.
+
+.push() takes one or more parameters and "pushes" them onto the end of the array.
+
+*/
+
+const myArray = [["John", 23], ["cat", 2]];
+myArray.push(["dog", 3]);
