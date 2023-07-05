6f93eba88f78ef337dc13c2e7e7cba80c8ca844c
MerrilBds <Bdsmerril@gmail.com>
2023-07-05 14:49:03 +0200

    leçon 31 fin

---
+++ #31.variables(string_assign_with_variable)/append.js
@@ -0,0 +1,7 @@
+/*
+Just as we can build a string over multiple lines out of string literals, 
+we can also append variables to a string using the plus equals (+=) operator.
+*/
+const someAdjective = "incredible";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
