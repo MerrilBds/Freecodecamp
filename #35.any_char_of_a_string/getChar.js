a47fd22b94c3369637eb05b39298e21714bb4776
MerrilBds <Bdsmerril@gmail.com>
2023-07-05 15:08:59 +0200

    leçon 35 fin

---
+++ #35.any_char_of_a_string/getChar.js
@@ -0,0 +1,10 @@
+/*
+You can also use bracket notation to get the character at other positions within a string.
+
+Remember that computers start counting at 0, so the first character is actually the zeroth character.
+
+*/
+const lastName = "Lovelace";
+
+
+const thirdLetterOfLastName = lastName[2]; 
