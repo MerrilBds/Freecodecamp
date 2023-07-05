d8ec376a225a88c23950902f048e7686f0e36521
MerrilBds <Bdsmerril@gmail.com>
2023-07-05 16:42:28 +0200

    leçon 46 fin

---
+++ #46.array_shift()Method/shift.js
@@ -0,0 +1,4 @@
+//That's where .shift() comes in. It works just like .pop(), except it removes the first element instead of the last.
+const myArray = [["John", 23], ["dog", 3]];
+
+const removedFromMyArray = myArray.shift();
