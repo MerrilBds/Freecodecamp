49965dbab17e1098fa7602b91b79e86302103b7b
MerrilBds <Bdsmerril@gmail.com>
2023-07-05 14:41:51 +0200

    leçon 30 fin

---
+++ #30.variables(string_concat_with_variable)/concat.js
@@ -0,0 +1,6 @@
+/*
+Sometimes you will need to build a string. By using the concatenation operator (+), 
+you can insert one or more variables into a string you're building.
+*/
+const myName = "Merril";
+const myStr = "My names is " + myName + " and I am well!";
