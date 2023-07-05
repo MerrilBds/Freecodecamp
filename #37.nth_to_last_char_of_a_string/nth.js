62717928af30a9cf716660cc342e2a9e1eb5a243
MerrilBds <Bdsmerril@gmail.com>
2023-07-05 15:14:55 +0200

    leçon 37 fin

---
+++ #37.nth_to_last_char_of_a_string/nth.js
@@ -0,0 +1,8 @@
+//You can use the same principle we just used to retrieve the last character 
+//in a string to retrieve the Nth - to - last character.
+
+
+const lastName = "Lovelace";
+
+
+const secondToLastLetterOfLastName = lastName[lastName.length - 2];
