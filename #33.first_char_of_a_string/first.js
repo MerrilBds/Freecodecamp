a4773f4f2bdf17dd5e4eeffcdbe32acfa9ddfb93
MerrilBds <Bdsmerril@gmail.com>
2023-07-05 14:57:03 +0200

    leçon 33 fin

---
+++ #33.first_char_of_a_string/first.js
@@ -0,0 +1,7 @@
+//Bracket notation is a way to get a character at a specific index within a string.
+
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+
+firstLetterOfLastName = lastName[0]; 
