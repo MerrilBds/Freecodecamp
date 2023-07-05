29a577073306effaef3f0aee9f9f7c73eaf28c0b
MerrilBds <Bdsmerril@gmail.com>
2023-07-05 14:29:47 +0200

    leçon 29 fin

---
+++ #29.variables(string_assign_addition)/assign.js
@@ -0,0 +1,7 @@
+/*
+We can also use the += operator to concatenate a string onto the end of an existing string variable. 
+This can be very helpful to break a long string over several lines.
+*/
+let myStr = "This is the first sentence.";
+myStr += " This is the second sentence.";
+console.log(myStr);
