521304c79f7f9ab455194f00d99d10572c4484dd
MerrilBds <Bdsmerril@gmail.com>
2023-07-06 13:37:52 +0200

    leçon 108 fin

---
+++ #108.parseInt()/convert.js
@@ -0,0 +1,10 @@
+/*
+Use the parseInt Function
+
+The parseInt() function parses a string and returns an integer.
+*/
+function convertToInteger(str) {
+    return parseInt(str);
+}
+
+convertToInteger("56");
