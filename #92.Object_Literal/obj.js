c85e5c7e912cb94b6b9de2778a2d5a291c4dc18a
MerrilBds <Bdsmerril@gmail.com>
2023-07-06 12:01:06 +0200

    leçon 92 fin

---
+++ #92.Object_Literal/obj.js
@@ -0,0 +1,21 @@
+/*
+Sometimes you may want to store data in a flexible Data Structure.
+ A JavaScript object is one way to handle flexible data. 
+They allow for arbitrary combinations of strings, numbers, booleans, arrays, functions, and objects.
+*/
+const myMusic = [
+    {
+        artist: "Billy Joel",
+        title: "Piano Man",
+        release_year: 1973,
+        formats: ["CD", "8T", "LP"],
+        gold: true
+    },
+
+    {
+        artist: "Deep Purple",
+        title: "Smoke on the water",
+        release_year: 1976,
+        formats: ["CD", "8T", "LP"]
+    }
+];
