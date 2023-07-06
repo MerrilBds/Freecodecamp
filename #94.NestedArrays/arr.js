a9371f1b644bc9c6ff88dd41dcca10922ea603ee
MerrilBds <Bdsmerril@gmail.com>
2023-07-06 12:10:53 +0200

    leçon 94 fin

---
+++ #94.NestedArrays/arr.js
@@ -0,0 +1,22 @@
+// Similar to accessing nested objects, array bracket notation can be chained to access nested arrays.
+
+const myPlants = [
+    {
+        type: "flowers",
+        list: [
+            "rose",
+            "tulip",
+            "dandelion"
+        ]
+    },
+    {
+        type: "trees",
+        list: [
+            "fir",
+            "pine",
+            "birch"
+        ]
+    }
+];
+
+const secondTree = myPlants[1].list[1];
